fb160746fb6c122ba3e9c25a7df3836ae55632ab
Mohammad Hussein <114811218+mhmdhussein@users.noreply.github.com>
2025-08-22 11:48:59 +0300

    Create permutations.c

---
+++ permutations.c
@@ -0,0 +1,81 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_factorial(int n)
+{
+	if (n <= 1)
+		return 1;
+	return (n * ft_factorial(n - 1));
+}
+
+void generate_all_perms(int current_index, int size, char *s, char **all_perms, int *perms_row_index)
+{
+	if (current_index == size)
+	{
+		strcpy(all_perms[(*perms_row_index)], s);
+		(*perms_row_index)++;
+		return ;
+	}
+	for (int i = current_index; i < size; i++)
+	{
+		char temp = s[i];
+		s[i] = s[current_index];
+		s[current_index] = temp;
+		generate_all_perms(current_index + 1, size, s, all_perms, perms_row_index);
+		temp = s[i];
+		s[i] = s[current_index];
+		s[current_index] = temp;
+	}
+}
+
+void sort_perms(char **all_perms, int total_perms)
+{
+	for (int i = 0; i < total_perms; i++)
+	{
+		for (int j = i + 1; j < total_perms; j++)
+		{
+			if (strcmp(all_perms[i], all_perms[j]) > 0)
+			{
+				char *temp = all_perms[i];
+				all_perms[i] = all_perms[j];
+				all_perms[j] = temp;
+			}
+		}
+	}
+}
+
+void print_perms(char **all_perms, int total_perms)
+{
+	for (int i = 0; i < total_perms; i++)
+		puts(all_perms[i]);
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		write(1, "\n", 1);
+		return (1);
+	}
+	char *s = av[1];
+	int size = strlen(s);
+	if (size == 0)
+	{
+		write(1, "\n", 1);
+		return (1);
+	}
+	int total_perms = ft_factorial(size);
+
+	char **all_perms = malloc(sizeof(char *) * total_perms);
+
+	for (int i = 0; i < total_perms; i++)
+		all_perms[i] = calloc(size + 1, sizeof(char));
+
+	int perms_row_index = 0;
+	int current_index = 0;
+	generate_all_perms(current_index, size, s, all_perms, &perms_row_index);
+	sort_perms(all_perms, total_perms);
+	print_perms(all_perms, total_perms);
+	return (0);
+}
